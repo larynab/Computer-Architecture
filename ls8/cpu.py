880bb68024a09b8b8a1c8d2e6f7fcc7f2aa6ecce
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-07-25 13:36:31 -0700

    multiplication

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,10 @@
 
 import sys
 
-
-
 HLT = 0b00000001
 PRN = 0b01000111
 LDI = 0b10000010
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -31,8 +30,6 @@ class CPU:
         
         address = 0
 
-        # For now, we've just hardcoded a program:
-
         if len(sys.argv) != 2:
             print(f"usage: {sys.argv[0]}")
             sys.exit(1)
@@ -45,33 +42,25 @@ class CPU:
                         continue
                     self.ram[address] = int(num[0:8], 2)
                     address += 1
-                    print(int(num))
+                    print(int(num[0:8]))
 
 
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {sys.argv[1]} not found")
             sys.exit(2)
 
-        # f = open(sys.argv[1],"r")
-        # program = f.read()
-        # # f.close()
-        # print(program) 
-        # program = sys.argv[1]
-
         # for instruction in program:
         #     self.ram[address] = instruction
         #     address += 1
-
-
-
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.register[reg_a] *= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -103,16 +92,31 @@ class CPU:
             IR = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc+1)
             operand_b = self.ram_read(self.pc+2)
-
             if IR == LDI:
                 self.register[operand_a] = operand_b
+                self.pc += 3
+
+            if IR == MUL:
+                print("MULTIPLYING")
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
 
             if IR == PRN:
                 print(f"PRN {self.register[operand_a]}")
-
+                self.pc += 2
+                
             elif IR == HLT:
                 running = False
-
-            self.pc += 1
-                
+                print("HLT")
+                self.pc += 1
+            # self.pc += 1
+            # else:
+            #     running = False
+            #     break
+            #     print("no more commands, closing")  
+            
+    # def LDI(self):
+    #     operand_a = self.ram_read(self.pc+1)
+    #     operand_b = self.ram_read(self.pc+2)
+    #     self.register[operand_a] = operand_b              
 
